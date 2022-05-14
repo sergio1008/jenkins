5f26aca23a86644139a12777f1f6d404f26f4b18
sergio1008 <sergiobernalc@outlook.com>
2022-05-14 03:41:50 -0500

    Add files via upload

---
+++ src/test/java/com/serbernal/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serbernal.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
